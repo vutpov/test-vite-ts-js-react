4cd4bf6ff36161f5a0cafccbc7f674bfa9ee8759
vutpov <vuthpov26@gmail.com>
2024-03-29 09:40:02 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Counter from "./Counter.js";
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+//@ts-ignore
+import fs from "fs/promises";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+});
